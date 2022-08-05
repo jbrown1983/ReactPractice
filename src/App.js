2c964e9a1c7b2a06db1257c9e9e63d489697952c
J.Brown <99134463+jbrown1983@users.noreply.github.com>
2022-08-04 23:07:38 -0500

    "first"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExenseItem></ExenseItem> 
+    </div>
+  );
+}
+
+export default App;
